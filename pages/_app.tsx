809434331cd6c4976e89df3942966696c218793c
Jack <k4667333@gmail.com>
2024-09-19 14:18:56 +0800

    debug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
 // import nextI18NextConfig from '../next-i18next.config.js'
-import { Footer } from "../components/Footer";
 import localFont from "next/font/local";
 import dynamic from 'next/dynamic';
 const Header = dynamic(() => import('../components/Header'), { ssr: false });
